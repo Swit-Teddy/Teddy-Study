1114d8cb4224205b3abf9c5f466e10efa247933a
Teddy <teddy@swit.io>
2022-02-06 21:41:40 +0900

    [Add] B Branch Test

--- GIFinTextViewPrx/GIFinTextViewPrx/Source/Scene/CHChatList/Controller/ViewController.swift
+++ GIFinTextViewPrx/GIFinTextViewPrx/Source/Scene/CHChatList/Controller/ViewController.swift
@@ -61,6 +61,10 @@ class ViewController: UIViewController {
     func setUI(){
         self.view.addSubview(tableView)
     }
+    
+    func confilctTestFunc(){
+          print("B confilct func 입니당")
+      }
 }
 
 
